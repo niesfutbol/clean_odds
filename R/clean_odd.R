b2710de3ac958e3e7597b037a3660189e19e5080
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-02-08 18:34:26 -0800

    ♻️ Refactor

--- R/clean_odd.R
+++ R/clean_odd.R
@@ -6,12 +6,12 @@ name_bet <- "Match Winner"
 extract_bets <- function(full_file) {
   match_winner <- full_file[["bookmakers"]][[1]][["bets"]][[1]][["values"]]
   expected <- tibble::tibble(
-    "match_id" = match_id,
-    "update_odd" = update_odd,
+    "match_id" = full_file[["fixture"]][["id"]],
+    "update_odd" = full_file[["update"]] |> stringr::str_sub(1, 10),
     "value_bet" = purrr::map_chr(match_winner, ~ .$value),
     "odd_bet" = as.double(purrr::map_chr(match_winner, ~ .$odd)),
-    "name_bet" = name_bet,
-    "house_bets" = house_bets
+    "name_bet" = full_file[["bookmakers"]][[1]][["bets"]][[1]][["name"]],
+    "house_bets" = full_file[["bookmakers"]][[1]][["name"]]
   )
   return(expected)
 }
