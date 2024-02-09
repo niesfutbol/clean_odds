7e0b19d41a200c08b56e1c915d25c7723b88e3f3
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-02-08 18:22:08 -0800

    ♻️ Refactor

--- R/clean_odd.R
+++ R/clean_odd.R
@@ -2,17 +2,14 @@ match_id <- 881972
 update_odd <- "2023-01-28"
 house_bets <- "Bet365"
 name_bet <- "Match Winner"
-value_bet <- c("Home", "Draw", "Away")
-odd_bet <- c(6.50, 4.33, 1.50)
 
-# purrr::map_chr(full_file[["bookmakers"]][[1]][["bets"]][[1]][["values"]], ~ .$value)
 extract_bets <- function(full_file) {
   match_winner <- full_file[["bookmakers"]][[1]][["bets"]][[1]][["values"]]
   expected <- tibble::tibble(
     "match_id" = match_id,
     "update_odd" = update_odd,
     "value_bet" = purrr::map_chr(match_winner, ~ .$value),
-    "odd_bet" = odd_bet,
+    "odd_bet" = as.double(purrr::map_chr(match_winner, ~ .$odd)),
     "name_bet" = name_bet,
     "house_bets" = house_bets
   )
