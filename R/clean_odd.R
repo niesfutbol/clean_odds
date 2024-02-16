64013f42c57fbd200b32879882b366da25a12235
Nepo Rojas <braulio.rojas@islas.org.mx>
2024-02-15 20:11:28 -0800

    ♻️ Extract function

--- R/clean_odd.R
+++ R/clean_odd.R
@@ -4,7 +4,7 @@ extract_bets <- function(full_file) {
     "match_id" = full_file[["fixture"]][["id"]],
     "update_odd" = full_file[["update"]] |> stringr::str_sub(1, 10),
     "value_bet" = purrr::map_chr(match_winner, ~ .$value),
-    "odd_bet" = as.double(purrr::map_chr(match_winner, ~ .$odd)),
+    "odd_bet" = .extract_odd_bet(match_winner),
     "name_bet" = full_file[["bookmakers"]][[1]][["bets"]][[1]][["name"]],
     "house_bets" = .extract_house_bets(full_file)
   )
@@ -13,4 +13,8 @@ extract_bets <- function(full_file) {
 
 .extract_house_bets <- function(full_file) {
   return(full_file[["bookmakers"]][[1]][["name"]])
+}
+
+.extract_odd_bet <- function(match_winner) {
+  as.double(purrr::map_chr(match_winner, ~ .$odd))
 }
